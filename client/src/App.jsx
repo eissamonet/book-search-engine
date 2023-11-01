7c31a7127f921a001924f7c65e9aae03db50bec1
Eissa Purnell <eissamonet@gmail.com>
2023-10-31 22:17:42 -0500

    connection to graphql added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,39 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
 
+import SearchBooks from './pages/SearchBooks';
+import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
+
+//establish apollo client
+const client = new ApolloClient({
+  request: operation => {
+    const token = localStorage.getItem('id_token');
+
+    operation.setContext({
+      headers: {
+        authorization: token ? token : ''
+      }
+    });
+  },
+  //establish connection to graphql server
+  uri: '/graphql'
+});
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Outlet />
-    </>
+    <ApolloProvider client={client}>
+      <Router>
+        <>
+          <Navbar />
+          <Switch>
+            <Route exact path='/' component={SearchBooks} />
+            <Route exact path='/saved' component={SavedBooks} />
+            <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
+          </Switch>
+        </>
+      </Router>
+    </ApolloProvider>
   );
 }
 
